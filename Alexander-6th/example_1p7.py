cfb135a82f117f3a63aa1217ea6c54ebab99c603
Hao Jin <hjin.zj@gmail.com>
2019-09-08 10:55:52 +0800

    power

---
+++ Alexander-6th/example_1p7.py
@@ -0,0 +1,12 @@
+p1 = -5*20  # W
+p2 = 5*12
+p3 = 6*8
+p4 = -1*8
+
+print("Calculated power for each element.")
+print("Positive and negative value means absorbed and supplied power, respectively.")
+print(f"p1: {p1} W")
+print(f"p2: {p2} W")
+print(f"p3: {p3} W")
+print(f"p4: {p4} W")
+print(f"p1 + p2 + p3 + p4 should equals to 0: {p1 + p2 + p3 + p4} W")
---
+++ Alexander-6th/pp_1p7.py
@@ -0,0 +1,12 @@
+p1 = -9*5  # W
+p2 = 9*2
+p3 = 4*0.6*5
+p4 = 5*3
+
+print("Calculated power for each element.")
+print("Positive and negative value means absorbed and supplied power, respectively.")
+print(f"p1: {p1} W")
+print(f"p2: {p2} W")
+print(f"p3: {p3} W")
+print(f"p4: {p4} W")
+print(f"p1 + p2 + p3 + p4 should equals to 0: {p1 + p2 + p3 + p4} W")
